737477ddafe130327ffada6b9c366d3a0858dcba
kris <ksowatta@uci.edu>
2025-12-25 21:15:14 -0800

    Completed Day 6 Part 1 + 2

--- day6/day6.py
+++ day6/day6.py
@@ -20,37 +20,37 @@ def part_1(file):
     return grand_total
 
 def part_2(file):
-    matrix = None
-    grand_total = 0
+    numbers = []
     for line in file:
-        numbers = line.split(' ')
-        numbers = [number for number in numbers if number.strip()]
-        if matrix is None:
-            matrix = [[int(number)] for number in numbers]
-        elif numbers[0] in ['*', '+']:
-            for i, operation in enumerate(numbers):
-                # Re-order the numbers correctly
-                wrong_numbers = matrix[i]
-                correct_numbers = []
-                while sum(wrong_numbers) != 0:
-                    correct_number = 0
-                    for i in range(len(wrong_numbers)):
-                        correct_number = correct_number * 10 + wrong_numbers[i]%10 if wrong_numbers[i] != 0 else correct_number
-                        wrong_numbers[i] = wrong_numbers[i]//10
-                    correct_numbers.append(correct_number)
-                print(correct_numbers)
-                if operation == '*':
-                    grand_total += math.prod(correct_numbers)
-                else:
-                    grand_total += sum(correct_numbers)
+        if line[0] not in ['*', '+']:
+            for i, char in enumerate(line):
+                if len(numbers) < i + 1:
+                    numbers.append([])
+                numbers[i] += char
         else:
-            for i, number in enumerate(numbers):
-                matrix[i].append(int(number))
+            print(numbers)
+            operators = [char for char in line if char in ['*', '+']]
+            number_index = 0
+            grand_total = 0
+            for operator in operators:
+                current_total = 1 if operator == '*' else 0
+                while number_index < len(numbers):
+                    number = ''.join(numbers[number_index])
+                    number_index += 1
+                    if number.strip() == '':
+                        break
+                    else:
+                        if operator == '*':
+                            current_total *= int(number)
+                        else:
+                            current_total += int(number)
+                grand_total += current_total
+
     return grand_total
 
 
 if __name__ == "__main__":
     INPUT = 'input.txt'
     EXAMPLE = 'example.txt'
-    with open(EXAMPLE) as file:
+    with open(INPUT) as file:
         print(part_2(file))
