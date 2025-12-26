25e1fb3585b3c7c024616de813a877675141f6f7
kris <ksowatta@uci.edu>
2025-12-25 22:21:48 -0800

    Brute Force Day 7 Part 2, need to optimize

--- day7/day7.py
+++ day7/day7.py
@@ -1,23 +1,36 @@
+import copy
+
 def part_1(file):
     matrix = []
     for line in file:
         line = line.strip()
         matrix.append([char for char in line])
-    splits = 0
-    for i, line in enumerate(matrix):
-        if i > 0:
-            for j in range(len(matrix[0])):
+    
+    def get_timelines(i, matrix):
+        timelines = 0
+        while i < len(matrix):
+            j = 0
+            while j < len(matrix[0]):
                 # Check for S
                 if matrix[i-1][j] in ['S', '|']:
                     if matrix[i][j] != '^':
                         matrix[i][j] = '|'
                     else:
-                        splits += 1
                         if j > 0 and matrix[i][j-1] != '^':
-                            matrix[i][j-1] = '|'
+                            # Left timeline
+                            left_matrix = copy.deepcopy(matrix)
+                            left_matrix[i][j-1] = '|'
+                            timelines += get_timelines(i+1, left_matrix)
                         if j < len(matrix[0]) - 1 and matrix[i][j+1] != '^':
+                            # Right timeline
                             matrix[i][j+1] = '|'
-    return splits
+                            timelines += get_timelines(i+1, matrix)
+                        return timelines
+                j += 1
+            i += 1
+        return 1
+    
+    return get_timelines(1, matrix)
 
 if __name__ == "__main__":
     INPUT = 'input.txt'
